b88a5012228ddea5a8b264e36b7475f733e1427d
Chawanrat Gaysornkaew <6310611014@cn102>
2022-09-15 23:04:54 +0700

    add background

--- AS2/subject/urls.py
+++ AS2/subject/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from users import views as u_views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path("<int:subject_id>", views.subject, name="subject")
+    path("<int:subject_id>", views.subject, name="subject"),
+    path('users', u_views.index, name="users")
 ]
--- AS2/users/urls.py
+++ AS2/users/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from subject import views as s_views
 
 urlpatterns = [
     path('', views.index, name='u_index'),
     path('login', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
+    path('subject', s_views.index, name='subject')
 ]
